a1b802eddf7dc8f7ea4534a8b6bc420df04ff4ad
Taylor McKay <jtmckay@gmail.com>
2021-07-26 11:32:41 -0600

    Updates

---
+++ build/precache-manifest.6ec5226e65b0ea909a16b05b1ad28c99.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53fc071720a3b8ff39c07c5639607d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f67e74421080b0d71bab",
+    "url": "/static/js/2.b0cff965.chunk.js"
+  },
+  {
+    "revision": "3b47b5d417d3541c8126b84b2a684f84",
+    "url": "/static/js/2.b0cff965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3064164b9f942e6f3d09",
+    "url": "/static/js/main.fec0dae4.chunk.js"
+  },
+  {
+    "revision": "524bc46bac3608f4a324",
+    "url": "/static/js/runtime-main.2ea6c94b.js"
+  },
+  {
+    "revision": "1b0cd87ff7ad7a4980ed1e53a0eaa645",
+    "url": "/static/media/anvil.1b0cd87f.svg"
+  },
+  {
+    "revision": "db67bc189023c3106571a9678872091a",
+    "url": "/static/media/crown.db67bc18.svg"
+  },
+  {
+    "revision": "b0e0859bfc7996a061416f76b5e15781",
+    "url": "/static/media/easel.b0e0859b.svg"
+  },
+  {
+    "revision": "11581762ce4e3bf75f3895da55ff6a65",
+    "url": "/static/media/pickaxe.11581762.svg"
+  }
+]);
